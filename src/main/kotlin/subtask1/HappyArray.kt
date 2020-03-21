e2452d8c5f9e12f7f46c4d5da123ac65b0c7bd2e
LisPI <paschka.lis@gmail.com>
2020-03-21 17:44:40 +0300

    feat: task 1 (new version with predicate)

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -7,23 +7,36 @@ class HappyArray {
         if(sadArray.size < 3)
             return sadArray
 
-        var isArrChanged: Boolean
+//        var isArrChanged: Boolean
         var happyArray = sadArray
-        do{
-            isArrChanged = false
-            for((ind, elem) in happyArray.withIndex()){
-                if((ind == happyArray.lastIndex) or (ind == 0))
-                    continue
-
-                if(elem > happyArray[ind-1] + happyArray[ind+1]) {
-                    isArrChanged = true
-                    happyArray = happyArray.filterIndexed { index, _ -> index != ind }.toIntArray()
-                    break
-                }
-            }
-
-        } while (isArrChanged)
+        var prevSize : Int
+        do {
+            prevSize = happyArray.size
+            happyArray = happyArray.filterIndexed { index, i -> happyPredicate(index, i, happyArray) }
+                .toIntArray()
+        }while (prevSize != happyArray.size)
+//        do{
+//            isArrChanged = false
+//            for((ind, elem) in happyArray.withIndex()){
+//                if((ind == happyArray.lastIndex) or (ind == 0))
+//                    continue
+//
+//                if(elem > happyArray[ind-1] + happyArray[ind+1]) {
+//                    isArrChanged = true
+//                    happyArray = happyArray.filterIndexed { index, _ -> index != ind }.toIntArray()
+//                    break
+//                }
+//            }
+//
+//        } while (isArrChanged)
 
         return happyArray
+    }
+
+    private fun happyPredicate(index: Int, i: Int, h : IntArray): Boolean{
+        if((index == h.lastIndex) or (index == 0))
+            return true
+
+        return i <= h[index-1] + h[index+1]
     }
 }

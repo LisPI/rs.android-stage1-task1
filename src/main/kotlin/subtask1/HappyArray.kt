018f149d26781836a39687dd4bc0181dc7360615
LisPI <paschka.lis@gmail.com>
2020-03-21 14:02:29 +0300

    init: start task

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,6 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        return intArrayOf()
     }
 }

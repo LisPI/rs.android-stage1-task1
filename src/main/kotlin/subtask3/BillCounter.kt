dbd3375fbcabd63bbda708727c3835cd785ad5a5
LisPI <paschka.lis@gmail.com>
2020-03-21 18:43:55 +0300

    feat: task 3 (odd numbers?)

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -2,9 +2,15 @@ package subtask3
 
 class BillCounter {
 
-    // TODO: Complete the following function
+    private val fairlySplit = "bon appetit"
+
+    // TODO: odd numbers??
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        if(bill.isEmpty())
+            return fairlySplit
+
+        val fairlyBill = (bill.sum()- bill[k])/2
+        return if (fairlyBill == b) fairlySplit else (b - fairlyBill).toString()
     }
 }

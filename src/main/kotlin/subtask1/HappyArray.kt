195962b37b8ed40aa49023bdf3d96ba552d7f221
LisPI <paschka.lis@gmail.com>
2020-03-21 17:10:23 +0300

    feat: task 1 (bad code)

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,28 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
+    // TODO: fix bad code)
     fun convertToHappy(sadArray: IntArray): IntArray {
-        return intArrayOf()
+        if(sadArray.size < 3)
+            return sadArray
+
+        var isArrChanged: Boolean
+        var happyArray = sadArray
+        do{
+            isArrChanged = false
+            for((ind, elem) in happyArray.withIndex()){
+                if((ind == happyArray.lastIndex) or (ind == 0))
+                    continue
+
+                if(elem > happyArray[ind-1] + happyArray[ind+1]) {
+                    isArrChanged = true
+                    happyArray = happyArray.filterIndexed { index, _ -> index != ind }.toIntArray()
+                    break
+                }
+            }
+
+        } while (isArrChanged)
+
+        return happyArray
     }
 }

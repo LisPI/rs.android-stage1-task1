824c052193e9a5ed352d5add5940ec8a471f8593
LisPI <paschka.lis@gmail.com>
2020-03-22 11:43:56 +0300

    feat: task 5 (complete solution)

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -4,13 +4,37 @@ class HighestPalindrome {
 
     // TODO: lucky testing
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        when(digitString){
-            "2842" -> return "2882"
-            "092282" -> return "992299"
-            "58346739679" -> return "-1"
 
+        if(n<1)
+            return if(k<1) digitString else "9"
+        if(n==0)
+            return digitString
+
+        val halfsize = digitString.length/2
+        var kRequired = 0
+
+        for(i in 0 until halfsize){
+            if(digitString[i] != digitString[digitString.lastIndex-i])
+                kRequired++
         }
+        if(kRequired > k)
+            return "-1"
 
-        return "-1"
+        var reserve = k - kRequired
+        var builder = StringBuilder(digitString)
+        for(i in 0 until halfsize){
+            if(builder[i] != builder[builder.lastIndex-i]){
+                if(reserve > 0){
+                    builder[i] = '9'
+                    builder[builder.lastIndex-i] = '9'
+                    --reserve
+                }
+                else{
+                    builder[i] = maxOf(builder[i], builder[builder.lastIndex-i])
+                    builder[builder.lastIndex-i] = maxOf(builder[i], builder[builder.lastIndex-i])
+                }
+            }
+        }
+        return builder.toString()
     }
 }
